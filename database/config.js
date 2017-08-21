dd681e72bc41f2ce1ced29b69cdf97e99a3876fa
CarlossCrispin <carlos.crispin.cc@gmail.com>
2017-08-21 13:48:04 -0500

    modificando home

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 var config = {
-    //mn "url": process.env.DB || "mongodb://carlos:crispin@ds131511.mlab.com:31511/condominio" 
+    //mn "url": process.env.DB || "mongodb://carlos:crispin@ds131511.mlab.com:31511/mundo" 
      "url": process.env.DB || "mongodb://127.0.0.1:27017/Mundo" 
      
  };
